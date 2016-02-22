ccac481f0aba98cffcca4f18a922fc775df4c82f
Nils Langner <contact@phmlabs.com>
2016-02-22 10:22:46 +0100

    added core bundle

--- src/CoreBundle/DependencyInjection/Configuration.php
+++ src/CoreBundle/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace KoalamonIntegration\WebhookBundle\DependencyInjection;
+namespace KoalamonIntegration\CoreBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('koalamon_integration_webhook');
+        $rootNode = $treeBuilder->root('koalamon_integration_core');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
